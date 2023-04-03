b85ad847fc01f181db1f9d543220cf0c850f2502
Basam Hassan <basammoses@gmail.com>
2023-04-03 13:26:45 -0400

    updated urls

--- web_project/urls.py
+++ web_project/urls.py
@@ -32,6 +32,7 @@ routers.register('users', views.UserViewSet)
 
 
 urlpatterns = [
+  
     path('admin/', admin.site.urls),
     path('', include(routers.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
