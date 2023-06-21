465e26b417951b613ea62620eefb1f3aa8bf8ec9
donkeykong718 <122040169+donkeykong718@users.noreply.github.com>
2023-06-21 12:07:07 -0400

    Allows CORS

--- web_project/settings.py
+++ web_project/settings.py
@@ -224,7 +224,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = (
     "http://localhost:3000", "https://asl-front-git-dev-donkeykong718.vercel.app/", "https://asl-front.vercel.app/", "https://asl-back.herokuapp.com/"
 )
